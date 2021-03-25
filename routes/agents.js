3322ddc1ed80c88507cd61bf0b205fcc3bdf0fa1
William <williamzabo@gmail.com>
2021-03-25 14:26:39 +0100

    addAgent

---
+++ routes/agents.js
@@ -0,0 +1,22 @@
+"use strict"
+const sql=require('../lib/sqlfunction')
+const express = require('express')
+let router = express.Router()
+
+router.
+route('/add')
+.get((req,res)=>{
+ 
+    res.send("get agent")
+  })
+.post((req,res)=>{
+    console.log(req.body)
+    res.send("post agent")
+    /** TODO send html result request */
+    
+    /** TODO : SQL REQUEST ADD AGENT */
+    sql.createAgent(req.body.Prenom, req.body.Nom, req.body.Objectif, req.body.statut )
+
+  })
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,43 @@
+require('dotenv').config
+const sql=require('./lib/sqlfunction')
+const fs = require('fs')
+const express = require('express')
+const app=express()
+const agents=require("./routes/agents")
+const router = express.Router();
+
+/** DB CONNECTION */
+app.use(express.urlencoded({ extended: true }))
+
+
+/** ROUTES */
+app.use('/agents', agents)
+app.use('/', router)
+
+
+app.post('/', (req,res)=>{
+  console.log(req.body)
+})
+
+
+app.put('/',(req,res)=>{
+  console.log(req.body)
+})
+
+
+app.get('/', function(req, res) {
+  fs.readFile('./html/index.html', (err, data) => {
+        if (err) {
+          res.writeHead(404)
+          res.end("Ce fichier n'existe pas")
+        }
+        res.writeHead(200, {
+          'Content-type':'text/html; charset=utf-8'
+        })
+        res.end(data)
+      })
+});
+
+
+//SERVER BASE
+app.listen(3000, () => console.log("server started on 3000"))
